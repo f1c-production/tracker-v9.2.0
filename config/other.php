6d1213ae09b0580c5b9d5013f551da446ee11881
Mochomo <f1carreras@proton.me>
2026-03-12 08:28:11 -0400

    Update other.php

--- config/other.php
+++ config/other.php
@@ -188,7 +188,7 @@ return [
     | True/1 = Enabled
     | False/0 = Disabled
     */
-    'application_signups' => false,
+    'application_signups' => true,
 
     /*
     |--------------------------------------------------------------------------
